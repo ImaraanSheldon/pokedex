5c06489588029481b147e5a3bd928a82568563df
ImaraanSheldon <imaraansheldonis2302003@gmail.com>
2025-01-25 20:13:44 +0200

    landing temp

--- pokemon/main.py
+++ pokemon/main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def landing():
     return render_template("index.html")
 
+@app.route("/home.html/")
+def home():
+    return render_template("home.html")
+
 if __name__ == "__main__":
     app.run(debug = True)
